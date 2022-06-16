9cfe93cc1f56293c39d6c470c6f6822f566d2da6
mindeleven <mindeleven@posteo.de>
2022-06-16 14:28:15 +0200

    Added network configurations to hardhat

--- hardhat-tutorial-code-along/hardhat.config.js
+++ hardhat-tutorial-code-along/hardhat.config.js
@@ -5,4 +5,15 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      // Chain ID of MetaMask Localhost 8545 configuration
+      // run 'ganache-cli --port 8545 --chainId 5777' first
+      chainId: 5777 // 1337
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9719668b397f4a14928a14b0dd205cd7",
+      accounts: [process.env.pk]
+    },
+  }
 };
