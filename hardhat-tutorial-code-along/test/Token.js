c546d5be6bba1cb15bbdd62dc31cf1e17217ce8a
mindeleven <mindeleven@posteo.de>
2022-06-16 11:14:31 +0200

    Renamed test directory and added second test

--- hardhat-tutorial-code-along/test/Token.js
+++ hardhat-tutorial-code-along/test/Token.js
@@ -23,3 +23,23 @@ describe("Token contract", function () {
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   })
 });
+
+describe("Transactions", function() {
+  it("Should transfer tokens between accounts", async function() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    // transfer 50 tokens from owner to addr1
+    // contract is depolyed be owner 
+    // owner is sender of transfer
+    await hardhatToken.transfer(addr1.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+    // switching between accounts and transfering 50 tokens from addr1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+  });
+});
