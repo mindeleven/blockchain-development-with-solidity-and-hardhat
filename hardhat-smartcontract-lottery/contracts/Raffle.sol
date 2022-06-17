759e108bbb80042b1439717e5eb50d2baa9c9fcf
mindeleven <mindeleven@posteo.de>
2022-06-17 10:04:28 +0200

    Added smart contract file with basic structure and to do list as comments

---
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Raffle contract TODOs
+// (1) Enter the lottery by paying some amount
+// (2) Pick a random winner (verifiable random)
+// (2) Winner to be selected completely automated every X minutes
+
+// What needs to be created outside of the contract:
+// (1) random number -> Chainlink Oracle, Randomness
+// (2) automated execution -> Chainlink Keepers
+
+contract Raffle {
+
+}
