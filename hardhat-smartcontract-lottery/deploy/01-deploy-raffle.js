729e3e2db59dde7a8060df81761cc593c37da65c
mindeleven <mindeleven@posteo.de>
2022-07-07 10:55:06 +0200

    Beginning to work on the configuration for a mock up of the integrated Chainlink contracts if the app is deployed to harthad or localhost

---
+++ hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
@@ -0,0 +1,15 @@
+const { getNamedAccounts, deployments, network, run } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const arguments = []
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+}
