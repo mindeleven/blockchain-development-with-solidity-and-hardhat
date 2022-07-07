1536aa03a3463ec2571a98109c8de7dd98f43c5f
mindeleven <mindeleven@posteo.de>
2022-07-07 10:03:54 +0200

    Some modifications to the network configuration

--- hardhat-smartcontract-lottery/hardhat.config.js
+++ hardhat-smartcontract-lottery/hardhat.config.js
@@ -9,7 +9,7 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+// const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
@@ -29,7 +29,7 @@ module.exports = {
             saveDeployments: true,
             chainId: 4,
             blockConfirmations = 6,
-            accounts: [PRIVATE_KEY]
+            accounts: [process.env.pk], // accounts: [PRIVATE_KEY]
       },
   },
   solidity: "0.8.7",
