eeea36233e207c9a02298a77a08233cd8678065e
mindeleven <mindeleven@posteo.de>
2022-07-07 09:38:59 +0200

    Setting network configurations for hardhat

--- hardhat-smartcontract-lottery/hardhat.config.js
+++ hardhat-smartcontract-lottery/hardhat.config.js
@@ -8,11 +8,30 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+          // // If you want to do some forking, uncomment this
+          // forking: {
+          //   url: MAINNET_RPC_URL
+          // }
+          chainId: 31337,
+          blockConfirmations = 1,
+      },
+      rinkeby: {
+            url: RINKEBY_RPC_URL,
+            saveDeployments: true,
+            chainId: 4,
+            blockConfirmations = 6,
+            accounts: [PRIVATE_KEY]
+      },
+  },
   solidity: "0.8.7",
   getNamedAccounts: {
     deployer: {
