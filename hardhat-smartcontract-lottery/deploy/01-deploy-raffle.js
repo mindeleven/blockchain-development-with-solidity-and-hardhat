2fb51aad69557957af988b0c1d89a8efb77eb8b5
mindeleven <mindeleven@posteo.de>
2022-07-08 10:15:53 +0200

    Creating a subscriptionId mock for the hardhat testnet, adding the subscriptionId to the args array for deployment

--- hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
+++ hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
@@ -1,17 +1,28 @@
 const { getNamedAccounts, deployments, network, run } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        // create subscription ID mock
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait()
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription
+        // Usually, you'd need the link token on a real network
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
@@ -20,6 +31,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       vrfCoordinatorV2Address,
       entranceFee,
       gasLane,
+      subscriptionId,
     ]
 
     const raffle = await deploy("Raffle", {
