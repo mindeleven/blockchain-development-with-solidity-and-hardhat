cf3e5b4c534ff572e36bebbbe3153917f8c0541f
mindeleven <mindeleven@posteo.de>
2022-07-08 09:45:30 +0200

    Setting the entranceFee as a parameter for deployment and integrating a networkConfig for hardhat

--- hardhat-smartcontract-lottery/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -1,15 +1,24 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     4: {
         name: "rinkeby",
+        // contract address taken from Chainlink documentation
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ether.utils.parseEther("0.01"),
         // subscription id from https://vrf.chain.link/
         subscriptionId: "6726",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
-        // contract address taken from Chainlink documentation
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
     },
+    // hardhat config doesn't need vrfCoordinatorV2 address because it takes
+    // it from the mock
+    31337: {
+      name: "hardhat",
+      entranceFee: ether.utils.parseEther("0.01"),
+    }
 }
 
 const developmentChains = ["hardhat", "localhost"]
