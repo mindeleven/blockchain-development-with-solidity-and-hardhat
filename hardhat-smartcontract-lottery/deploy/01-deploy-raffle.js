ab23f95da14367d88d2af9092a7bf866a04e6d97
mindeleven <mindeleven@posteo.de>
2022-07-08 09:33:38 +0200

    Setting the vfrCoordinator address as a parameter depending on the network

--- hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
+++ hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
@@ -1,14 +1,24 @@
 const { getNamedAccounts, deployments, network, run } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
 
-    const arguments = []
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const args = []
 
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: arguments,
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
