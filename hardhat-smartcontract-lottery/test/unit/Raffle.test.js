8f32614cb82705b70a29b4ea307678660a527bad
mindeleven <mindeleven@posteo.de>
2022-07-10 12:26:32 +0200

    Scaffolding for writing unit tests

---
+++ hardhat-smartcontract-lottery/test/unit/Raffle.test.js
@@ -0,0 +1,3 @@
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
