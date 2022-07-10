86bd63f12a86b65c5fa90176b0f617fe1d3a17cc
mindeleven <mindeleven@posteo.de>
2022-07-10 15:34:58 +0200

    Connecting test functionality with the network, getting the contracts and testing for initial state and interval

--- hardhat-smartcontract-lottery/test/unit/Raffle.test.js
+++ hardhat-smartcontract-lottery/test/unit/Raffle.test.js
@@ -1,3 +1,35 @@
 const { assert, expect } = require("chai")
-const { network, deployments, ethers } = require("hardhat")
+const { getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", async function () {
+        let raffle, vrfCoordinatorV2Mock
+
+        beforeEach(async function(){
+            const deployer = await getNamedAccounts()
+            // deploys modules with the tags "mocks", "raffle" and "all"
+            await deployments.fixture(["all"])
+            // get a new connection to the Raffle contract
+            // connect it with deployer
+            raffle = await ethers.getContract("Raffle", deployer)
+            // get a new connection to the VRFCoordinatorV2Mock contract
+            vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+        })
+
+        describe("constructor", async function () {
+            it("intitiallizes the raffle correctly", async () => {
+                // Ideally, we'd separate these out so that only 1 assert per "it" block
+                // And ideally, we'd make this check everything
+                const raffleState = (await raffle.getRaffleState()).toString()
+                const interval = await raffle.getInterval()
+                // Comparisons for Raffle initialization:
+                assert.equal(raffleState, "0")
+                assert.equal(
+                    interval.toString(),
+                    networkConfig[network.config.chainId]["keepersUpdateInterval"]
+                )
+            })
+        })
+    })
