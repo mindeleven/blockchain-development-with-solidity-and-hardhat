fd646e4ca11cba9d1724ba8644d78361dfaed1fe
mindeleven <mindeleven@posteo.de>
2022-07-13 09:34:21 +0200

    Added unit test that asserts that true is returned if enough time has passed, has players, has eth and is open

--- hardhat-smartcontract-lottery/test/unit/Raffle.test.js
+++ hardhat-smartcontract-lottery/test/unit/Raffle.test.js
@@ -119,6 +119,14 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                 const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
                 assert(!upkeepNeeded)
             })
+
+            it("returns true if enough time has passed, has players, eth, and is open", async () => {
+                await raffle.enterRaffle({ value: raffleEntranceFee })
+                await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                await network.provider.request({ method: "evm_mine", params: [] })
+                const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                assert(upkeepNeeded)
+            })
         })
 
     })
