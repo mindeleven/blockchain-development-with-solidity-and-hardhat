375d9a5f32bb50ffeec9198b0eebde1cfdf9da28
mindeleven <mindeleven@posteo.de>
2022-07-13 09:30:23 +0200

    Added unit test that asserts that false is returned if raffle isn't open

--- hardhat-smartcontract-lottery/test/unit/Raffle.test.js
+++ hardhat-smartcontract-lottery/test/unit/Raffle.test.js
@@ -95,6 +95,22 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                 const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x")
                 assert(!upkeepNeeded)
             })
+
+            it("returns false if raffle isn't open", async () => {
+                await raffle.enterRaffle({ value: raffleEntranceFee })
+                await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                await network.provider.request({ method: "evm_mine", params: [] })
+                // change the state to calculate
+                await raffle.performUpkeep([])
+                // store the new state
+                const raffleState = await raffle.getRaffleState()
+                // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                // const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x")
+                const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
+                // assert.equal(raffleState.toString() == "1", upkeepNeeded == false)
+                assert.equal(raffleState.toString(), "1")
+                assert.equal(upkeepNeeded, false)
+            })
         })
 
     })
